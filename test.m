7c2cb0aa029e31e945f14bc43d7465c50d471c66
Alex-Oiko <alex.pana.oikonomou@gmail.com>
2012-12-05 17:45:43 +0000

    contour pictures

--- test.m
+++ test.m
@@ -13,27 +13,27 @@ axis square
 bot=load('init1.ctr');
 top=load('init2.ctr');
 
-l=0.3;
-%bot1=bot;	
-%top1=top;
-%sb=size(bot);
-%st=size(top);
-%for i=1:sb(1)
-%    bot1(i,1)=bot(i,1)+20;
-%    bot1(i,2)=bot(i,2)+20;
-%    top1(i,1)=top(i,1)-20;
-%    top1(i,2)=top(i,2)-20;
-%end
+l=0.8;
+bot1=bot;	
+top1=top;
+sb=size(bot);
+st=size(top);
+for i=1:sb(1)
+    bot1(i,1)=bot(i,1);
+    bot1(i,2)=bot(i,2)+50;
+    top1(i,1)=top(i,1);
+    top1(i,2)=top(i,2);
+end
 
-[position,energies,points] = produce_energies(im,bot,top,l);
+[position,energies,points] = produce_energies(im,bot1,top1,l);
 
 contour = get_contour(position,energies,points);
 hold on
 %plot(points(:,:,1),points(:,:,2),'r+')
 plot(contour(:,1),contour(:,2),'r+');
-plot(bot(:,1),bot(:,2),'g+-');
-plot(top(:,1),top(:,2),'g+-');
+%plot(bot(:,1),bot(:,2),'g+-');
+%plot(top(:,1),top(:,2),'g+-');
 
 
-%plot(bot1(:,1),bot1(:,2),'g+-');
-%plot(top1(:,1),top1(:,2),'g+-');
+plot(bot1(:,1),bot1(:,2),'g+-');
+plot(top1(:,1),top1(:,2),'g+-');
