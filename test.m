4c14e8c8847e40dd5eb2021916d4a30f72412441
Alex-Oiko <alex.pana.oikonomou@gmail.com>
2012-12-05 16:41:54 +0000

    this definately works. Watch out what parameters you pass in the produce_energies function

--- test.m
+++ test.m
@@ -12,28 +12,28 @@ axis square
 
 bot=load('init1.ctr');
 top=load('init2.ctr');
-%bot1=bot;
+
+l=0.3;
+%bot1=bot;	
 %top1=top;
 %sb=size(bot);
 %st=size(top);
 %for i=1:sb(1)
-%        bot1(i,1)=bot(i,1)+20;
-%        bot1(i,2)=bot(i,2)+20;
-%        top1(i,1)=top(i,1)-20;
-%        top1(i,2)=top(i,2)-20;
+%    bot1(i,1)=bot(i,1)+20;
+%    bot1(i,2)=bot(i,2)+20;
+%    top1(i,1)=top(i,1)-20;
+%    top1(i,2)=top(i,2)-20;
 %end
 
-l=0.9;
-top1
-bot1
-[position,energies,points] = produce_energies(im,bot1,top1,l);
+[position,energies,points] = produce_energies(im,bot,top,l);
 
 contour = get_contour(position,energies,points);
 hold on
 %plot(points(:,:,1),points(:,:,2),'r+')
 plot(contour(:,1),contour(:,2),'r+');
-%plot(bot(:,1),bot(:,2),'g+-');
-%plot(top(:,1),top(:,2),'g+-');
-
 plot(bot(:,1),bot(:,2),'g+-');
 plot(top(:,1),top(:,2),'g+-');
+
+
+%plot(bot1(:,1),bot1(:,2),'g+-');
+%plot(top1(:,1),top1(:,2),'g+-');
