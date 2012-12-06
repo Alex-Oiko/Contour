87ccc6ed851b5fced4e8d8cc140f12a9cbab4ac0
Alex-Oiko <alex.pana.oikonomou@gmail.com>
2012-12-06 02:31:48 +0000

    report under way.NEEDS:3.2 Scalability with changes in M. Different contour initialisations-->add an x case. Other tested images

--- test.m
+++ test.m
@@ -10,23 +10,40 @@ imagesc (im)
 colormap ( gray )
 axis square
 
+%szim=size(im)
+
+%bot=zeros(100,2);
+%top=zeros(100,2);
+
+%sb=size(bot);
+%xstep=szim(2)/sb(1);
+
+
+%for i=1:1:sb(1)
+%	bot(i,1)=xstep*i;
+%	bot(i,2)=170 -0.1*i;
+%	top(i,1)=xstep*i;
+%	top(i,2)=30-0.1*i;
+%end
+
 bot=load('init1.ctr');
 top=load('init2.ctr');
 
-l=0.8;
+l=0.5;
 bot1=bot;	
 top1=top;
 sb=size(bot);
 st=size(top);
 for i=1:sb(1)
-    bot1(i,1)=bot(i,1);
-    bot1(i,2)=bot(i,2)+50;
-    top1(i,1)=top(i,1);
+    bot1(i,1)=bot(i,1)+50;
+    bot1(i,2)=bot(i,2);
+    top1(i,1)=top(i,1)+50;
     top1(i,2)=top(i,2);
 end
 
-[position,energies,points] = produce_energies(im,bot1,top1,l);
+[position,energies,points] = produce_energies(im,bot,top,l);
 
+a=2
 contour = get_contour(position,energies,points);
 hold on
 %plot(points(:,:,1),points(:,:,2),'r+')
@@ -35,5 +52,5 @@ plot(contour(:,1),contour(:,2),'r+');
 %plot(top(:,1),top(:,2),'g+-');
 
 
-plot(bot1(:,1),bot1(:,2),'g+-');
-plot(top1(:,1),top1(:,2),'g+-');
+plot(bot(:,1),bot(:,2),'g+-');
+plot(top(:,1),top(:,2),'g+-');
